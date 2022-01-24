aa23254fc8eb0e9e6cc10939410ebe3c1c977cf0
Mehdi Chiboub <mehdichiboub.98@gmail.com>
2022-01-24 01:42:35 +0100

    Feat: KMeans implementation

--- src/main/java/project/nft/nft/controllers/CollectionController.java
+++ src/main/java/project/nft/nft/controllers/CollectionController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.*;
 import project.nft.nft.models.Collection;
 import project.nft.nft.services.CollectionService;
 
+import java.io.IOException;
 import java.util.List;
+
+import static project.nft.nft.controllers.KMeans.kMeanspp;
+import static project.nft.nft.controllers.KMeans.kmeans;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:4200"}, maxAge = 6000)
@@ -21,6 +25,26 @@ public class CollectionController {
         System.out.println(limit);
         System.out.println(page);
         return (List<Collection>) this.collectionService.list(limit, page);
+    }
+
+    @GetMapping("/analysis")
+    public String startAnalysis(@RequestParam("clusters") int k) {
+        System.out.println(k);
+        try {
+            // read data
+            DataSet data = new DataSet("files/sample.csv");
+            System.out.println(data.getAttrNames());
+            // remove (input any irrelevant attributes)
+            // data.removeAttr("Class");
+            // cluster
+            kmeans(data, k);
+            // output into a csv
+            data.createCsvOutput("files/sampleClustered.csv");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return "KMeans finished successfully";
     }
 
 }
